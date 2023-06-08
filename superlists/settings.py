cb575c9c02ee6327dcf3230b9f117396b3a184fb
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-06-08 17:59:16 +1000

    Edit settings to deploy to PythonAnywhere.com

--- superlists/settings.py
+++ superlists/settings.py
@@ -18,13 +18,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zl!0v^(70+a!t)4aergufvk)&n)0v^qlu25aq$_4r(m@wpd7ix'
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'django-insecure-h$zs=na!knsxvd$l)6+232*ohqpvw62a7v@ic2jt*pf!+qp%%='
+
+    ALLOWED_HOSTS = []  # blank = all allowed
 
 # Application definition
 
