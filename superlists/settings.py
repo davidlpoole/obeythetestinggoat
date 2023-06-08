425d7add623a307007173fdfa0cc2c98edc719ed
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-06-08 18:26:50 +1000

    update static root/url

--- superlists/settings.py
+++ superlists/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/davidpoole/obey/superlists/static'
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/home/davidpoole/obey/superlists/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
