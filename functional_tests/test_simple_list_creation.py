9203ca3d8d8bc3821815f5965aea00c91d9fdbde
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-06-11 14:54:34 +1000

    Fix functional test for header element
    also remove bootstrap static files (no longer used)

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -13,7 +13,7 @@ class NewVisitorTest(FunctionalTest):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(by='tag name', value='h1').text
+        header_text = self.browser.find_element(by='id', value='header').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
