d9d17ed456640039f7979881e4cf00d2f17eacb3
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-06-28 10:52:40 +0100

    Started creating a new test suite with full isolation

--- lists/views.py
+++ lists/views.py
@@ -14,7 +14,7 @@ def home_page(request):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
         list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
